1dcf9c9ac829fcf772644409de2e46e90f08de14
duke <18001870631@163.com>
2016-06-29 14:27:35 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Duke on 6/29/16.
+//  Copyright © 2016 Honeywell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Duke on 6/29/16.
+//  Copyright © 2016 Honeywell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
